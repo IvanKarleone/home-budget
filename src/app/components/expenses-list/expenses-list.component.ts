6aaed251a3d6054b011e57c43c26c528b617263c
Kirill Iliasov <kirill.ilyasov1997@gmail.com>
2024-10-24 13:41:03 +0200

    update angular config

--- src/app/components/expenses-list/expenses-list.component.ts
+++ src/app/components/expenses-list/expenses-list.component.ts
@@ -3,9 +3,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'app-expenses-list',
   standalone: true,
-  imports: [],
   templateUrl: './expenses-list.component.html',
-  styleUrl: './expenses-list.component.scss',
+  styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ExpensesListComponent {}
--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -4,7 +4,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'app-not-found',
   standalone: true,
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.scss',
+  styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NotFoundComponent {}
