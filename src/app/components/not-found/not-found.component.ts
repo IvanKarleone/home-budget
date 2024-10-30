d2e4f106f963f8da24eb0d0af77c290b9202767b
Kirill Iliasov <kirill.ilyasov1997@gmail.com>
2024-10-30 16:20:55 +0100

    update not-foud page

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,8 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TuiButton } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
+  imports: [RouterLink, TuiButton],
   templateUrl: './not-found.component.html',
   styles: `
     :host {
