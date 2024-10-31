35dab85c77cc248b58f7e7141847d8575791a384
Kirill Iliasov <kirill.ilyasov1997@gmail.com>
2024-10-31 13:09:29 +0100

    add expense

--- src/app/components/expenses-list/expenses-list.component.ts
+++ src/app/components/expenses-list/expenses-list.component.ts
@@ -1,14 +1,50 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TuiButton, TuiDataList, TuiDialog } from '@taiga-ui/core';
+import { TuiInputNumberModule, TuiSelectModule } from '@taiga-ui/legacy';
+import { TuiAutoFocus } from '@taiga-ui/cdk';
+import { TuiDataListWrapper } from '@taiga-ui/kit';
+
+export interface IExpense {
+  amount: number;
+  currency: 'RUB' | 'USD' | 'EUR';
+  date: Date;
+  category: 'products' | 'home' | 'travel' | 'other';
+}
+
+const currencies = ['RUB', 'USD', 'EUR'];
 
 @Component({
   selector: 'app-expenses-list',
   standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    TuiButton,
+    TuiDialog,
+    TuiInputNumberModule,
+    TuiAutoFocus,
+    TuiSelectModule,
+    TuiDataList,
+    TuiDataListWrapper,
+  ],
   templateUrl: './expenses-list.component.html',
   styles: `
     :host {
-      display: block;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      row-gap: 10px;
     }
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ExpensesListComponent {}
+export class ExpensesListComponent {
+  readonly expenseForm = new FormGroup({
+    amount: new FormControl(0, { nonNullable: true, validators: [Validators.required] }),
+    currency: new FormControl('RUB', { nonNullable: true, validators: [Validators.required] }),
+  });
+
+  currencies = currencies;
+
+  isOpenedDialog = false;
+}
