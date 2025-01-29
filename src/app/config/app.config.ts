8baaa0d5f6522b3bd2d9dc35a21b3fd33b636cda
Kirill Iliasov <kirill.ilyasov1997@gmail.com>
2025-01-29 17:32:59 +0100

    add angular/firebase package

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -1,4 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { routes } from '@app/routes/app.routes';
@@ -14,5 +17,17 @@ export const appConfig: ApplicationConfig = {
     tuiValidationErrorsProvider({
       required: 'Required to fill',
     }),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'home-budget-897c3',
+        appId: '1:346296040686:web:748d0249f3c490a86f07ff',
+        storageBucket: 'home-budget-897c3.firebasestorage.app',
+        apiKey: 'AIzaSyDampO4KDEv9kDRLOxTsBooMoCFU03XEqI',
+        authDomain: 'home-budget-897c3.firebaseapp.com',
+        messagingSenderId: '346296040686',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideStorage(() => getStorage()),
   ],
 };
