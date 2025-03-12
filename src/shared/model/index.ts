f74d295eb07ee02cff6ec274224384ca80fe8841
Kirill Iliasov <kirill.ilyasov1997@gmail.com>
2025-03-12 17:14:14 +0100

    add alert service in index.ts

--- src/shared/model/index.ts
+++ src/shared/model/index.ts
@@ -1,3 +1,4 @@
+export { AlertService } from './alert/alert.service';
 export { BreakpointService } from './breakpoint/breakpoint.service';
 export { ConfirmDialogService } from './dialog/confirm-dialog/confirm-dialog.service';
 export { ModalDialogService } from './dialog/modal-dialog/modal-dialog.service';
